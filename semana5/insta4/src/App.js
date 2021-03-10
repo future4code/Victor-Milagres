bd592fc9c094de680969b37f220506a431452cde
Victor Milagres <victor.pmilagres@gmail.com>
2021-03-10 16:09:41 -0300

    Componentes de Classe + Estados

---
+++ semana5/insta4/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Post from './components/Post/Post';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={'app-container'}>
+        <Post
+          nomeUsuario={'Aline'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'Ortega'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'Zizou'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import './SecaoComentario.css'
+
+export class SecaoComentario extends Component {
+	state = {
+		inputComentario: ''
+	}
+
+	onChangeComentario = (event) => {
+		console.log(event.target.value)
+	  	this.setState({inputComentario: event.target.value})
+	  }
+
+	render() {
+		return <div className={'comment-container'}>
+			<input
+				className={'input-comentario'}
+				placeholder={'Comentário'}
+				value={this.state.inputComentario}
+				onChange={this.onChangeComentario}
+			/>
+			<button onClick={this.props.aoEnviar}>Enviar</button>
+		</div>
+	}
+}
