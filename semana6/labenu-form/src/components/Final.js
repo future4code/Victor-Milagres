eca06f96d30a8301528cf6543283fddebcbe61ba
Victor Milagres <victor.pmilagres@gmail.com>
2021-03-15 17:19:43 -0300

    Renderização Condicional

---
+++ semana6/labenu-form/src/components/Final.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PerguntaAberta(props) {
+  return (
+    <div>
+      <h2>O FORMULÁRIO ACABOU</h2>
+      <h3>Muito obrigado por participar! Entraremos em contato!</h3>
+    </div>
+  );
+}
